b0ae2019c4ddf5bf783204f0499689a27b033e8f
4rg0n <argonthechecker@gmail.com>
2023-10-16 13:54:24 +0200

    add demo spring boot app; remove .idea folder from version control

---
+++ src/test/java/vizardalpha/songsofspirit/translation/TranslationApplicationTests.java
@@ -0,0 +1,13 @@
+package vizardalpha.songsofspirit.translation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
